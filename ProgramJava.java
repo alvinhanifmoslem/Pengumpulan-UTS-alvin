b30f798b45117726b544dce36a70d6b0842d5eb9
alvinhanifmoslem <81188135+alvinhanifmoslem@users.noreply.github.com>
2021-04-22 08:41:57 +0700

    Add files via upload

---
+++ ProgramJava.java
@@ -0,0 +1,19 @@
+package UTS;
+
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.Int;
+
+public class ProgramJava {
+    public static void main (String []args)
+    {
+        String name1 = "JoeSmith";
+        String name2 = "MarySharp";
+        int age1     = 24 ;
+        int age2     = 52 ;
+
+        System.out.println(name1);
+        System.out.println(age1);
+        System.out.println(name2);
+        System.out.println(age2);
+    }
+
+}
